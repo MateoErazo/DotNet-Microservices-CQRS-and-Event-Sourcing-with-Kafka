7bc5de4c1957eecf21a82ad9ec7416cb511d57cd
MateoErazo <erazojesusmateo@gmail.com>
2023-11-02 15:54:00 -0500

    I add PostRepository

--- Post.Query.Api/Program.cs
+++ Post.Query.Api/Program.cs
@@ -8,6 +8,9 @@ Action<DbContextOptionsBuilder> configureDbContext = (o => o.UseLazyLoadingProxi
 builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
 builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
 
+//Create database and tables from code
+var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
+dataContext.Database.EnsureCreated();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
